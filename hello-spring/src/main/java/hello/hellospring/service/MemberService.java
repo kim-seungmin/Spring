b145ff668566eb847ba0bc28a64be76501864b9c
kim-seungmin <ksm981204@naver.com>
2022-03-01 17:59:17 +0900

    add aop

--- hello-spring/src/main/java/hello/hellospring/service/MemberService.java
+++ hello-spring/src/main/java/hello/hellospring/service/MemberService.java
@@ -37,15 +37,8 @@ public class MemberService {
                 });
     }
 
-    public List<Member> findMembers() {
-        long start = System.currentTimeMillis();
-        try {
-            return memberRepository.findAll();
-        } finally {
-            long finish = System.currentTimeMillis();
-            long timeMs = finish - start;
-            System.out.println("findMembers " + timeMs + "ms");
-        }
+    public List<Member> findMembers(){
+        return memberRepository.findAll();
     }
 
     public Optional<Member> findOne(Long memberId){
